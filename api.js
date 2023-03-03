82bb13b862961d6d814a71c62faf7d18706a6897
1871mg <1871mg@gmail.com>
2023-03-03 14:55:03 +0300

    за 4 урока fix

--- api.js
+++ api.js
@@ -1,31 +1,31 @@
 class CatsApi {
-    constructor(apiName) {
-        this.url = `https://cats.petiteweb.dev/api/single/${apiName}`
-    }
+  constructor(apiName) {
+    this.url = `https://cats.petiteweb.dev/api/single/${apiName}`
+  }
 
-    getAllCats() {
-        return fetch(`${this.url}/show`)
-    }
+  getAllCats() {
+    return fetch(`${this.url}/show`)
+  }
 
-    getCurrentCat(id) {
-        return fetch(`${this.url}/show/${id}`)
-    }
+  getCurrentCat(id) {
+    return fetch(`${this.url}/show/${id}`)
+  }
 
-    deleteCat(id) {
-        return fetch(`${this.url}/delete/${id}`, {
-            method: 'DELETE',
-        })
-    }
+  deleteCat(id) {
+    return fetch(`${this.url}/delete/${id}`, {
+      method: 'DELETE',
+    })
+  }
 
-    addNewCat(data) {
-        return fetch(`${this.url}/add`, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-    }
+  addNewCat(data) {
+    return fetch(`${this.url}/add`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+  }
 }
 
 const DB_NAME = '1871mg';
--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const $formErrorMsg = document.querySelector('[data-errmsg]')
 const HIDDEN_CLASS = 'hidden'
 
 const generateCatCard = (cat) => {
-    return (
-        `<div data-card_id=${cat.id} class="card mx-2" style="width: 18rem">
+  return (
+    `<div data-card_id=${cat.id} class="card mx-2" style="width: 18rem">
         <img
           src="${cat.image}"
           class="card-img-top"
-          alt="фоточка котенка"
+          alt="фото киски"
         />
         <div class="card-body">
           <h5 class="card-title">${cat.name}</h5>
@@ -26,95 +26,95 @@ const generateCatCard = (cat) => {
 }
 
 $wrapper.addEventListener('click', async (event) => {
-    const action = event.target.dataset.action;
+  const action = event.target.dataset.action;
 
-    switch (action) {
-        case 'delete':
-            const $currentCard = event.target.closest('[data-card_id]');
-            const catId = $currentCard.dataset.card_id;
-            try {
-                const res = await api.deleteCat(catId);
-                const responce = await res.json();
-                if (!res.ok) throw Error(responce.message)
-                $currentCard.remove()
-            } catch (error) {
-                console.log(error);
-            }
-            break;
+  switch (action) {
+    case 'delete':
+      const $currentCard = event.target.closest('[data-card_id]');
+      const catId = $currentCard.dataset.card_id;
+      try {
+        const res = await api.deleteCat(catId);
+        const responce = await res.json();
+        if (!res.ok) throw Error(responce.message)
+        $currentCard.remove()
+      } catch (error) {
+        console.log(error);
+      }
+      break;
 
-        case 'open':
-            // открывается модалка где расположена подробная информация о коте
-            // должен происходить какой запрос на бек о всей информации о конкретном коте по id
-            // вывести в модальном окне
-            break;
+    case 'open':
+      // открывается модалка где расположена подробная информация о коте
+      // должен происходить какой запрос на бек о всей информации о конкретном коте по id
+      // вывести в модальном окне
+      break;
 
-        case 'edit':
-            // открывается модалка с формой
-            // должен происходить какой запрос на бек о всей информации о конкретном коте по id
-            // форма уже предзаполнена информацией о коте
-            break;
+    case 'edit':
+      // открывается модалка с формой
+      // должен происходить какой запрос на бек о всей информации о конкретном коте по id
+      // форма уже предзаполнена информацией о коте
+      break;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 })
 
 $addBtn.addEventListener('click', () => {
-    $modalAdd.classList.remove(HIDDEN_CLASS) // открываем модалку
+  $modalAdd.classList.remove(HIDDEN_CLASS) // открываем модалку
 })
 
 document.forms.add_cats_form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    $formErrorMsg.innerText = '';
+  event.preventDefault();
+  $formErrorMsg.innerText = '';
 
-    const data = Object.fromEntries(new FormData(event.target).entries());
+  const data = Object.fromEntries(new FormData(event.target).entries());
 
-    data.id = Number(data.id)
-    data.age = Number(data.age)
-    data.rate = Number(data.rate)
-    data.favorite = data.favorite == 'on'
+  data.id = Number(data.id)
+  data.age = Number(data.age)
+  data.rate = Number(data.rate)
+  data.favorite = data.favorite == 'on'
 
-    const res = await api.addNewCat(data)
+  const res = await api.addNewCat(data)
 
-    if (res.ok) {
-        $wrapper.replaceChildren();
-        getCatsFunc()
-        $modalAdd.classList.add(HIDDEN_CLASS)
-        return event.target.reset()
-    } else {
-        const responce = await res.json();
-        $formErrorMsg.innerText = responce.message
-        return;
-    }
+  if (res.ok) {
+    $wrapper.replaceChildren();
+    getCatsFunc()
+    $modalAdd.classList.add(HIDDEN_CLASS)
+    return event.target.reset()
+  } else {
+    const responce = await res.json();
+    $formErrorMsg.innerText = responce.message
+    return;
+  }
 })
 
 const getCatsFunc = async () => {
-    $spinner.classList.remove(HIDDEN_CLASS)
-    const res = await api.getAllCats();
+  $spinner.classList.remove(HIDDEN_CLASS)
+  const res = await api.getAllCats();
 
-    if (res.status !== 200) {
-        const $errorMessage = document.createElement('p');
-        $errorMessage.classList.add('error-msg');
-        $errorMessage.innerText = 'Произошла ошибка, попробуйте выполнить запрос позже';
+  if (res.status !== 200) {
+    const $errorMessage = document.createElement('p');
+    $errorMessage.classList.add('error-msg');
+    $errorMessage.innerText = 'Произошла ошибка, попробуйте выполнить запрос позже';
 
-        return $wrapper.appendChild($errorMessage);
-    }
+    return $wrapper.appendChild($errorMessage);
+  }
 
-    const data = await res.json();
+  const data = await res.json();
 
-    if (data.length === 0) {
-        const $notificationMessage = document.createElement('p');
-        $notificationMessage.innerText = 'Список котов пуст, добавьте первого котика';
+  if (data.length === 0) {
+    const $notificationMessage = document.createElement('p');
+    $notificationMessage.innerText = 'Список котяток пуст, добавьте котятку';
 
-        return $wrapper.appendChild($notificationMessage);
-    }
+    return $wrapper.appendChild($notificationMessage);
+  }
 
-    setTimeout(() => {
-        $spinner.classList.add(HIDDEN_CLASS)
-        data.forEach(cat => {
-            $wrapper.insertAdjacentHTML('afterbegin', generateCatCard(cat))
-        });
-    }, 1000);
+  setTimeout(() => {
+    $spinner.classList.add(HIDDEN_CLASS)
+    data.forEach(cat => {
+      $wrapper.insertAdjacentHTML('afterbegin', generateCatCard(cat))
+    });
+  }, 1000);
 }
 getCatsFunc();
 
@@ -126,4 +126,4 @@ getCatsFunc();
 // обработать все ошибки со всех запросов
 // окультурить спиннер
 // сделать красивой страницу
-// сделать мобильную верстку
+// сделать мобильную верстку 
